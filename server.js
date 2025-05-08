330c61cd8ce4cbc5748f629fd8da796bdab598b8
DHJ99 <dananjayamadushanka825@gmail.com>
2025-05-08 12:59:04 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const app = require('/app');
 const port = 3000;
 const host = '127.0.0.1';
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(XPathExpression.json());
+
+const uri = 'mongodb+srv://dana:Gm374@cluster0.kq1cggv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () =>{
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log('MongoDB Error : ' + error);
+    }
+};
+
+connect();
 
 const server = app.listen(port , host , () =>{
     

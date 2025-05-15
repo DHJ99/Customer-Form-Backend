86fa6b3c85fdda79e1a05f32b526cf408a9aafe5
DHJ99 <dananjayamadushanka825@gmail.com>
2025-05-15 10:42:38 +0530

    new funct

--- controller.js
+++ controller.js
@@ -4,10 +4,4 @@ const getUser= (cb) =>{
     cb(users);
 };
 
-const getUserById = () =>{
-    const user = users.find(user => user.id == id);
-    cd(user);
-};
 
-exports.getUsers = getUser;
-exports.getUserById = getUserById;

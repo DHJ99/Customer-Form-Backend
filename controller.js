d55db6b920acfb5485b6e0a3f64ee5a9924fa63e
DHJ99 <dananjayamadushanka825@gmail.com>
2025-05-15 10:25:35 +0530

    model

--- controller.js
+++ controller.js
@@ -1,17 +1,4 @@
-const users = [
-    {
-        id : 1,
-        name: 'Amal'
-    },
-    {
-        id:2,
-        name:'Bimal'
-    },
-    {
-        id:3 ,
-        name: 'Nimal'
-    }
-];
+
 
 const getUser= (cb) =>{
     cb(users);
